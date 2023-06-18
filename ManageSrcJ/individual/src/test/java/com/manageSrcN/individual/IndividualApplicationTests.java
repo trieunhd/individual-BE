7f80b68fb46e397d630e9557a529e7f7d2cf9175
MinhAn <minhanlefz09@gmail.com>
2023-06-18 11:00:29 +0700

    base src

---
+++ ManageSrcJ/individual/src/test/java/com/manageSrcN/individual/IndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manageSrcN.individual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
